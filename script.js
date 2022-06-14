bee716c02b53539cf0fc4f9ca93f8dfcff905046
Joshieee Cute! <100410460+JoshieeeCute@users.noreply.github.com>
2022-06-14 17:02:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+/*gsap.to(".preload-box", {
+	height: "100px",
+	width: "100px",
+	delay: 0.5,
+	duration: 1.8
+});*/
+
+gsap.to(".loader-box", {
+	delay: 0.5,
+	rotate: 225,
+	duration: 1.4,
+	repeat: -1,
+	repeatDelay: .5,
+	ease: "back.inOut"
+});
